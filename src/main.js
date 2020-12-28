dd9ad1f583b692dd33cbeb315635ac980129ebf1
Dievano Dantas <dievano_dantas@hotmail.com>
2020-12-28 15:36:10 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import Phaser from 'phaser';
+
+// import HelloWorldScene from './scenes/HelloWorldScene';
+import GameScene from './scenes/GameScene';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    audio:{
+        disableWebAudio: true
+    },
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 }
+        }
+    },
+    scene: [GameScene]
+};
+
+export default new Phaser.Game(config);
